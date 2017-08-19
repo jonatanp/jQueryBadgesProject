e5251c244b9c7d3eba06da569959175e40b6778b
jonatan.pedrosa <jonatan.pedrosa@mediatree.fr>
2017-08-19 11:25:35 +0100

    finish tuto

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,20 @@
-$(function() {
+$(function () {
 
-  // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/jonatanpedrosa.json',
+    dataType: 'jsonp',
+    success: function (response) {
+      response.courses.completed.forEach(function (element) {
+        console.log(element);
+        var item = $('<div class="course"></div>');
+        item.append('<h3>' + element.title + '</h3>')
+          .append('<img src="' + element["badge"] + '"/>')
+          .append('<a class="btn btn-primary" target="_blank" href="' + element["url"] + '">' +
+            'See Course' +
+            '</a>');
+        $("#badges").append(item);
+      });
+    }
+  });
 
 });
